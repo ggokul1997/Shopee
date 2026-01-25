69ef4298f40ff7888837a43093469718f1b5d3d5
G Gokul <ggokul1997@gmail.com>
2026-01-25 23:36:22 +0530

    before register after logout

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,19 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
  
 dotenv.config() 
 connectDB();
   
 const app = express();   
+
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(cookieParser());
 
  
 app.get('/', (req, res) => {
@@ -16,6 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
