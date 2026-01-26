742189c94978dec57eb9dad433a6f11f471b1e74
G Gokul <ggokul1997@gmail.com>
2026-01-26 15:36:25 +0530

    login and logout frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import ProductScreen from './screens/ProductScreen';
 import { Provider } from 'react-redux';
 import store from './store';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
        <Route index path='/' element={<HomeScreen />} />
        <Route path='/product/:id' element={<ProductScreen />} />
         <Route path='/cart' element={<CartScreen />} />
+        <Route path='/login' element={<LoginScreen />} />
     </Route>
   )
 )
