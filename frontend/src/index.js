2f887751e68ba433f3c3edb7ab2e47672f655405
G Gokul <ggokul1997@gmail.com>
2026-01-28 20:24:51 +0530

    After profile and orderlist creation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,54 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css'; 
-import {createBrowserRouter,createRoutesFromElements, RouterProvider,Route} from "react-router-dom"
-import './assets/styles/bootstrap.custom.css'; 
-import './assets/styles/index.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import { Provider } from 'react-redux';
-import store from './store';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
-import { Privateroute } from './components/Privateroute';
-import PaymentScreen from './screens/PaymentScreen';
-import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Provider } from "react-redux";
+import store from "./store";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import { Privateroute } from "./components/Privateroute";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import ProfileScreen from "./screens/ProfileScreen";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-       <Route index path='/' element={<HomeScreen />} />
-       <Route path='/product/:id' element={<ProductScreen />} />
-        <Route path='/cart' element={<CartScreen />} />
-        <Route path='/login' element={<LoginScreen />} />
-        <Route path='/register' element={<RegisterScreen />} />
-       
+    <Route path="/" element={<App />}>
+      <Route index path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
 
-        <Route path='/' element={<Privateroute />}>
-          {/* Protected Routes go here */}
-     <Route path='/shipping' element={<ShippingScreen />} />
-     <Route path='/payment' element={<PaymentScreen />} />
-     <Route path='/placeorder' element={<PlaceOrderScreen />} /> 
-        </Route>
-        
-    </Route>
-  )
-)
+      <Route path="/" element={<Privateroute />}>
+        {/* Protected Routes go here */}
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
+    </Route>,
+  ),
+);
 root.render(
   <React.StrictMode>
-  <Provider store={store}>  
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,5 +1,5 @@
 import {apiSlice} from './apiSlice';
-import { ORDERS_URL } from '../constants';
+import { ORDERS_URL,PAYPAL_URL } from '../constants';
 
 
 
@@ -11,10 +11,36 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
                 method: 'POST',
                 body: { ...order },
             }),
-        })
+        }),
+         getOrderDetails: builder.query({
+            query: (orderId) => ({
+                url: `${ORDERS_URL}/${orderId}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        payOrder: builder.mutation({
+            query: ({orderId, details}) => ({
+                url: `${ORDERS_URL}/${orderId}/pay`,
+                method: 'PUT',
+                body: { ...details },
+            }),
+        }),
+        getPayPalClientId: builder.query({
+            query: () => ({
+                url: PAYPAL_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/mine`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+
     }))
 })
 
 
 
-export const {useCreateOrderMutation} = ordersApiSlice;
+export const {useCreateOrderMutation,useGetOrderDetailsQuery,usePayOrderMutation,useGetPayPalClientIdQuery,useGetMyOrdersQuery} = ordersApiSlice;
